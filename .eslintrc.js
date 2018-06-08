9767ff41455119f224bea8d4f6e5f12d0760ebae
Samuel Hayashi <samuelh@ysandpartners.com>
2018-06-09 01:22:41 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  "extends": "airbnb",
+  "installedESLint": true,
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "parserOptions": {
+    "sourceType": "script"
+  },
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jquery": true
+  },
+  "rules": {
+    "indent": ["error", 2],
+    "padded-blocks": "off",
+    "wrap-iife": ["error", "inside"],
+    "comma-dangle": ["error", "never"],
+    "func-names": "off",
+    "strict": ["error", "global"],
+    "prefer-arrow-callback": "off",
+    "no-underscore-dangle": "off"
+  },
+  "globals": {
+    // GSAP and ScrollMagic globals
+    "ScrollMagic": false,
+    "TimelineLite": false,
+    "TimelineMax": false,
+    "TweenLite": false,
+    "TweenMax": false,
+    "Back": false,
+    "Bounce": false,
+    "Circ": false,
+    "Cubic": false,
+    "Ease": false,
+    "EaseLookup": false,
+    "Elastic": false,
+    "Expo": false,
+    "Linear": false,
+    "Power0": false,
+    "Power1": false,
+    "Power2": false,
+    "Power3": false,
+    "Power4": false,
+    "Quad": false,
+    "Quart": false,
+    "Quint": false,
+    "RoughEase": false,
+    "Sine": false,
+    "SlowMo": false,
+    "SteppedEase": false,
+    "Strong": false,
+    "Draggable": false,
+    "SplitText": false,
+    "VelocityTracker": false,
+    "CSSPlugin": false,
+    "ThrowPropsPlugin": false,
+    "BezierPlugin": false
+  }
+};
