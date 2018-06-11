407cdef7acfff1135fcad43b2c9f60b92c0ad42e
Samuel Hayashi <samuelh@ysandpartners.com>
2018-06-11 10:59:36 +0900

    Add base ES6 code

--- source/assets/js/app.js
+++ source/assets/js/app.js
@@ -1,10 +1,24 @@
+(function(window, document) {
 
-// JavaScript
-window.sr = ScrollReveal(); 
+ $(function() {
 
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px' 
-// });
+
+    // ES6 closure (IIFE replacement)
+    {
+
+      console.log('Main file!');
+      console.log($('html'));
+
+      const es = [];
+      for (let i = 0; i < 10; i++) {
+        let c = i;
+        es[i] = () => console.log('Upcoming edition of ECMAScript is ES' + c);
+      }
+      es[6](); // Upcoming edition of ECMAScript is ES
+
+    }
+
+
+ });
+
+}(window, document));
